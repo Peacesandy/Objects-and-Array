d1b7cd60037ecb93523146a83d18d12ab135826f
Sandy <peacesandy04@gmail.com>
2023-02-28 09:12:50 -0800

    Finished Stutern Assignment

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Objects-and-Array
 Stutern assignment on Object and Array
+
+Question 4
+
+1. Using Spread
+    examples
+    const food = { beef: '🥩', bacon: '🥓' };
+
+const cloneFood = { ...food };
+
+console.log(cloneFood);
+// { beef: '🥩', bacon: '🥓' }
+
+2. Using Object.assign
+     examples
+     const food = { beef: '🥩', bacon: '🥓' };
+
+const cloneFood = Object.assign({}, food);
+
+console.log(cloneFood);
+// { beef: '🥩', bacon: '🥓' }
+
+
+3. Using JSON
+examples
+const food = { beef: '🥩', bacon: '🥓' };
+
+const cloneFood = Object.assign({}, food);
+
+console.log(cloneFood);
+// { beef: '🥩', bacon: '🥓' }
---
+++ index.js
@@ -0,0 +1,31 @@
+
+// Question One
+
+function InstagramPost(handle, content, image, link, views, likes){
+    this.handle = handle
+    this.content = content
+    this.image = image
+    this.link = link
+    this.views = views
+    this.likes = likes
+}
+
+let content = new content(food, vlog, tech, faith);
+let handleofauthor = new handleofauthor(ivy, toriah, peace)
+
+//Question 3
+
+function createPerson(name, age, location){
+        name = MusaDawodu
+         age = 19
+         location = lagos
+}
+
+function createJambScores(eng,govt,lit,crk){
+    eng - 70
+     govt - 85
+     lit - 82
+     crk - 94   
+}
+
+//Question 5
